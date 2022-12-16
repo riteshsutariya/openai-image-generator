e4130b2505b03e54d6a9bba4bcc6bf765f7ef845
riteshsutariya <riteshsutariya94@gmail.com>
2022-12-16 18:28:26 +0530

    changes in backend

--- backend/controllers/openaiContoller.js
+++ backend/controllers/openaiContoller.js
@@ -1,44 +1,45 @@
-const { Configuration, OpenAIApi } = require('openai');
-const { body, validationResult } = require('express-validator')
+const { Configuration, OpenAIApi } = require("openai");
+const { body, validationResult } = require("express-validator");
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-    console.log(req.body)
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+  console.log(req.body);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let { prompt, size, n } = req.body;
+  n = parseInt(n);
+  const imageSize =
+    size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n,
+      size: imageSize,
+    });
+    const imageUrl = response.data.data;
+    return res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
-    const { prompt, size, n } = req.body;
-    n = parseInt(n)
-    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
-    try {
-        const response = await openai.createImage({
-            prompt,
-            n,
-            size: imageSize
-        })
-        const imageUrl = response.data.data
-        return res.status(200).json({
-            success: true,
-            data: imageUrl
-        })
-    } catch (error) {
-        if (error.response) {
-            console.log(error.response.status)
-            console.log(error.response.data)
-        } else {
-            console.log(error.message)
-        }
-        return res.status(400).json({
-            success: false,
-            error: 'The image could not be generated'
-        })
-    }
-}
+    return res.status(400).json({
+      success: false,
+      error: "The image could not be generated",
+    });
+  }
+};
 
-module.exports = { generateImage }
+module.exports = { generateImage };
